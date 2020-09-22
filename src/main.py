1ea56ce68a74ef1848d11a819e75a1cb4b127c1b
wiktor779 <wiktor.bieda@gmail.com>
2020-09-23 01:05:34 +0200

    removing direct entries and none medium

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ from src.utils import *
 from src.first_and_last_touch_model import *
 
 df = load_data()
-df = remove_direct_entries(df, 'medium')
+df = remove_direct_entries(df)
 df = remove_outliers_z_score(df, 3.5)
 
 df = transform_utm_columns_into_list_of_strings(df)
