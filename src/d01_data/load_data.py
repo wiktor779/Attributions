e3dedf5a5b2b60784bc420e76eefa86ef9cb72b7
wiktor779 <wiktor.bieda@gmail.com>
2020-10-11 11:56:15 +0200

    refactored

---
+++ src/d01_data/load_data.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+
+def load_data(path_to_file='../../data/01_raw/conversion_paths.csv'):
+    df = pd.read_csv(path_to_file)
+
+    columns_ordered = [
+        'client_uuid',
+        'revenue',
+        'utm_source',
+        'utm_medium',
+        'event_dates',
+        'event_timestamps',
+        'days_till_conversions',
+        'conversion_date',
+        'conversion_timestamp',
+
+    ]
+    return df[columns_ordered]
