7aa0417d2f27ec9c91b66a6d6a08a0095119fca1
wiktor779 <wiktor.bieda@gmail.com>
2020-10-16 22:00:54 +0200

    last touch

--- src/d04_modelling/predict_last_touch.py
+++ src/d04_modelling/predict_last_touch.py
@@ -1,6 +1,9 @@
 import pandas as pd
-from src.d00_utils.utils import get_unique_values
+from collections import OrderedDict
+from src.d00_utils.utils import get_unique_values, remove_channel_from_path, split_data_into_train_and_test
 from src.d05_model_evaluation.measure_results import measure_results
+from src.d05_model_evaluation.measure_channels_impact import measure_channels_impact
+from src.d07_visualisation.visualise_channel_impact import visualize_channel_impact
 
 
 def predict_first_touch(df):
@@ -13,6 +16,12 @@ def predict_first_touch(df):
 
 if __name__ == "__main__":
     conversion_paths = pd.read_pickle('../../data/02_intermediate/cleaned.pkl')
-    revenue_predicted_last_touch = predict_first_touch(conversion_paths)
-    rmse = measure_results(conversion_paths.revenue, revenue_predicted_last_touch)
+    conversion_paths_train, conversion_paths_test = split_data_into_train_and_test(conversion_paths)
+    revenue_predicted_last_touch = predict_first_touch(conversion_paths_test)
+    rmse = measure_results(conversion_paths_test.revenue, revenue_predicted_last_touch)
     print(f'Last touch \tRMSE: {rmse}')
+
+    channels_impact = measure_channels_impact(conversion_paths_test, predict_first_touch)
+    visualize_channel_impact(channels_impact, 'last_touch')
+    print(channels_impact)
+
