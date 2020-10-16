4fa1d4d78a5b74b03a9ba7f6aaa768b9b2a26a88
wiktor779 <wiktor.bieda@gmail.com>
2020-10-16 22:11:20 +0200

    naive predicting - always predict mean

--- src/d04_modelling/predict_naive.py
+++ src/d04_modelling/predict_naive.py
@@ -1,16 +1,16 @@
 import pandas as pd
-from src.d01_data.load_data import load_data
+from src.d00_utils.utils import split_data_into_train_and_test
 from src.d05_model_evaluation.measure_results import measure_results
 
 
-def predict_naive(df):
-    revenue_mean = df.revenue.mean()
-    length = df.shape[0]
+def predict_naive(train, length):
+    revenue_mean = train.revenue.mean()
     return [revenue_mean]*length
 
 
 if __name__ == "__main__":
     conversion_paths = pd.read_pickle('../../data/02_intermediate/cleaned.pkl')
-    revenue_predicted_naive = predict_naive(conversion_paths)
-    rmse = measure_results(conversion_paths.revenue, revenue_predicted_naive)
-    print(f'RMSE: {rmse}')
+    conversion_paths_train, conversion_paths_test = split_data_into_train_and_test(conversion_paths)
+    revenue_predicted_naive = predict_naive(conversion_paths_train, conversion_paths_test.shape[0])
+    rmse = measure_results(conversion_paths_test.revenue, revenue_predicted_naive)
+    print(f'Naive \tRMSE: {rmse}')
