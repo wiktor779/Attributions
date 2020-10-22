25e5c55f21c23f3de8d9cf863e740c228f473547
wiktor779 <wiktor.bieda@gmail.com>
2020-10-22 20:04:41 +0200

    Lstm model (#6)
    * removing outliers
    
    * added encoding into 2d vector for lstm
    
    * added activation function and corrected metrics
    
    * added lstm predicting model
    
    * lstm train model
    
    * added mean absolute error

--- src/d04_modelling/model_lstm_train.py
+++ src/d04_modelling/model_lstm_train.py
@@ -1,18 +1,19 @@
 import pandas as pd
-from keras.layers import Dense
+from keras.layers import Dense, LSTM
 from keras.models import Sequential
 from src.d00_utils.utils import split_data_into_train_and_test
 from src.d05_model_evaluation.measure_results import measure_results
 
-model_name = 'dense_model_small'
+model_name = 'lstm_model'
 
 
-def create_model_dense():
+def create_model_lstm():
     model = Sequential()
-    model.add(Dense(60, activation='relu', input_shape=(60,)))
-    model.add(Dense(10, activation='relu'))
+    model.add(LSTM(32, activation='relu', input_shape=(10, 6)))
+    model.add(Dense(6, activation='relu'))
     model.add(Dense(1))
-    model.compile(optimizer="rmsprop", loss="mse", metrics=['mse', 'mae'])
+    model.summary()
+    model.compile(optimizer='rmsprop', loss="mse", metrics=['mse', 'mae'])
     return model
 
 
@@ -20,17 +21,18 @@ if __name__ == "__main__":
     conversion_paths = pd.read_pickle('../../data/03_processed/added_embedded_vector.pkl')
     conversion_paths_train, conversion_paths_test = split_data_into_train_and_test(conversion_paths)
 
-    x_train = conversion_paths_train['utm_medium_embedded'].to_list()
+    column_name = 'utm_medium_embedded_2d'
+    x_train = conversion_paths_train[column_name].to_list()
     y_train = conversion_paths_train.revenue.to_list()
-    x_test = conversion_paths_test['utm_medium_embedded'].to_list()
+    x_test = conversion_paths_test[column_name].to_list()
     y_test = conversion_paths_test.revenue.to_list()
 
-    model_dense = create_model_dense()
-    model_dense.fit(x_train, y_train, batch_size=32, epochs=5, validation_data=(x_test, y_test))
+    model_lstm = create_model_lstm()
+    model_lstm.fit(x_train, y_train, epochs=10, batch_size=32, validation_data = (x_test, y_test))
     filepath = f'../../data/04_models/{model_name}.h5'
-    model_dense.save(filepath)
+    model_lstm.save(filepath)
     print(f'Saved file to: {filepath}')
 
-    predicted_dense = model_dense.predict(x_test)
+    predicted_dense = model_lstm.predict(x_test)
     rmse = measure_results(y_test, predicted_dense)
-    print(f'Dense\tRMSE: {rmse}')
+    print(f'LSTM\tRMSE: {rmse}')
--- src/d05_model_evaluation/measure_results.py
+++ src/d05_model_evaluation/measure_results.py
@@ -1,8 +1,10 @@
 from math import sqrt
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_squared_error, mean_absolute_error
 
 
-def measure_results(revenue_actual, revenue_predicted):
+def measure_results(revenue_actual, revenue_predicted, mae=False):
     if len(revenue_actual) != len(revenue_predicted):
         raise Exception("Length of inputs lists should be the same!")
+    if mae:
+        return sqrt(mean_absolute_error(revenue_actual, revenue_predicted))
     return sqrt(mean_squared_error(revenue_actual, revenue_predicted))
