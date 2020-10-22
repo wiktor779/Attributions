b845f1f53a2a572477ce6ae40e366ff959b5dec9
wiktor779 <wiktor.bieda@gmail.com>
2020-10-22 19:16:06 +0200

    added encoding into 2d vector for lstm

--- src/d03_processing/create_model_input.py
+++ src/d03_processing/create_model_input.py
@@ -10,7 +10,7 @@ ENCODING_DIC_UTM_MEDIUM = {
 }
 
 
-def one_hot_encode_into_vector(path, encoding_dic, vector_length):
+def one_hot_encode_into_flat_vector(path, encoding_dic, vector_length):
     fill_to_desirable_length = len(encoding_dic) * (vector_length - len(path))
     vector = [0] * fill_to_desirable_length
     for touch in path:
@@ -18,7 +18,14 @@ def one_hot_encode_into_vector(path, encoding_dic, vector_length):
     return vector
 
 
-def transform_utm_into_vector(paths, encoding_dic, vector_length):
+def one_hot_encode_into_two_dim_vector(path, encoding_dic, vector_length):
+    vector = [[0] * len(encoding_dic)] * (vector_length - len(path))
+    for touch in path:
+        vector.append(encoding_dic[touch])
+    return vector
+
+
+def transform_utm_into_vector(paths, one_hot_encode_into_vector, encoding_dic, vector_length):
     """ takes vector_length last touches in path """
     paths_cropped = paths.apply(lambda path: path[-vector_length:])
     paths_embedded_vector = paths_cropped.apply(one_hot_encode_into_vector, args=(encoding_dic, vector_length))
@@ -26,9 +33,13 @@ def transform_utm_into_vector(paths, encoding_dic, vector_length):
 
 
 if __name__ == '__main__':
+    vector_length = 30
+    filename = f'added_embedded_vectors_{vector_length}'
     conversion_paths = pd.read_pickle('../../data/02_intermediate/cleaned.pkl')
     conversion_paths['utm_medium_embedded'] = transform_utm_into_vector(
-        conversion_paths.utm_medium_list, ENCODING_DIC_UTM_MEDIUM, 10)
-    filepath = '../../data/03_processed/added_embedded_vector.pkl'
+        conversion_paths.utm_medium_list, one_hot_encode_into_flat_vector, ENCODING_DIC_UTM_MEDIUM, vector_length)
+    conversion_paths['utm_medium_embedded_2d'] = transform_utm_into_vector(
+        conversion_paths.utm_medium_list, one_hot_encode_into_two_dim_vector, ENCODING_DIC_UTM_MEDIUM, vector_length)
+    filepath = f'../../data/03_processed/{filename}.pkl'
     conversion_paths.to_pickle(filepath)
     print(f'Saved file to: {filepath}')
