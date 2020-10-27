ea0b05a58dc43063f38240fb64a1fc3c97ec83b0
wiktor779 <wiktor.bieda@gmail.com>
2020-10-27 10:19:37 +0100

    calculate occurrences for raw and cleaned data

--- src/d06_reporting/calculate_pairs_occurrence.py
+++ src/d06_reporting/calculate_pairs_occurrence.py
@@ -2,7 +2,7 @@ from collections import Counter
 import pandas as pd
 
 
-def save_to_file_utm_source_and_medium_pairs_occurrence(df):
+def save_to_file_utm_source_and_medium_pairs_occurrence(df, filename):
     pairs = []
     for index, row in df.iterrows():
         for index_utm in range(len(row.utm_source_list)):
@@ -10,7 +10,7 @@ def save_to_file_utm_source_and_medium_pairs_occurrence(df):
             pairs.append(pair)
 
     c = Counter(pairs)
-    filepath = '../../results/utm_source_and_medium_pairs_occurrence.txt'
+    filepath = f'../../results/{filename}.txt'
     with open(filepath, 'w') as f:
         for k, v in c.most_common():
             f.write(f'{k} {v}\n')
@@ -19,5 +19,7 @@ def save_to_file_utm_source_and_medium_pairs_occurrence(df):
 
 
 if __name__ == '__main__':
+    conversion_paths = pd.read_pickle('../../data/02_intermediate/converted_into_lists.pkl')
+    save_to_file_utm_source_and_medium_pairs_occurrence(conversion_paths, 'utm_source_and_medium_pairs_occurrence_raw')
     conversion_paths = pd.read_pickle('../../data/02_intermediate/cleaned.pkl')
-    save_to_file_utm_source_and_medium_pairs_occurrence(conversion_paths)
+    save_to_file_utm_source_and_medium_pairs_occurrence(conversion_paths, 'utm_source_and_medium_pairs_occurrence_cleaned')
