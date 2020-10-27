a7e020ffe703cdd1fb8985b6e059c611e6b80708
wiktor779 <wiktor.bieda@gmail.com>
2020-10-27 15:13:12 +0100

    refactor plus added one hot encoding for utm source

--- src/d03_processing/create_model_input.py
+++ src/d03_processing/create_model_input.py
@@ -1,5 +1,17 @@
 import pandas as pd
 
+VECTOR_LENGTH = 40
+FILENAME = f'added_embedded_vectors_{VECTOR_LENGTH}'
+
+ENCODING_DIC_UTM_SOURCE = {
+    'google':       [1, 0, 0, 0],
+    'google.com':   [1, 0, 0, 0],
+    'facebook':     [0, 1, 0, 0],
+    'facebook.com': [0, 1, 0, 0],
+    'synerise':     [0, 0, 1, 0],
+    'bing.com':     [0, 0, 0, 1],
+}
+
 ENCODING_DIC_UTM_MEDIUM = {
     'cpc':      [1, 0, 0, 0, 0, 0],
     'e-mail':   [0, 1, 0, 0, 0, 0],
@@ -10,36 +22,62 @@ ENCODING_DIC_UTM_MEDIUM = {
 }
 
 
-def one_hot_encode_into_flat_vector(path, encoding_dic, vector_length):
-    fill_to_desirable_length = len(encoding_dic) * (vector_length - len(path))
-    vector = [0] * fill_to_desirable_length
-    for touch in path:
-        vector.extend(encoding_dic[touch])
-    return vector
+def _fill_beginning_with_zeros(row, utm_column_name, encoding_dic, is_2d, is_time):
+    path_length = len(row[utm_column_name])
+    embedded_vector = []
+    if path_length >= VECTOR_LENGTH:
+        return embedded_vector
+    embedded_touch = [0] * len(list(encoding_dic.values())[0])
+    if is_time:
+        embedded_touch.append(0)
+    for i in range(VECTOR_LENGTH - path_length):
+        if is_2d:
+            embedded_vector.append(embedded_touch)
+        else:
+            embedded_vector.extend(embedded_touch)
+
+    return embedded_vector
 
 
-def one_hot_encode_into_two_dim_vector(path, encoding_dic, vector_length):
-    vector = [[0] * len(encoding_dic)] * (vector_length - len(path))
-    for touch in path:
-        vector.append(encoding_dic[touch])
-    return vector
+def transform_into_vector(row, utm_column_name, encoding_dic, is_2d, is_time):
+    row[utm_column_name] = row[utm_column_name][-VECTOR_LENGTH:]
+    row['days_till_conversions_list'] = row['days_till_conversions_list'][-VECTOR_LENGTH:]
+    embedded_vector = _fill_beginning_with_zeros(row, utm_column_name, encoding_dic, is_2d, is_time)
 
+    for i, touch in enumerate(row[utm_column_name]):
+        embedded_touch = encoding_dic[touch].copy()
+        if is_time:
+            embedded_touch.append(row['days_till_conversions_list'][i])
+        if is_2d:
+            embedded_vector.append(embedded_touch)
+        else:
+            embedded_vector.extend(embedded_touch)
 
-def transform_utm_into_vector(paths, one_hot_encode_into_vector, encoding_dic, vector_length):
-    """ takes vector_length last touches in path """
-    paths_cropped = paths.apply(lambda path: path[-vector_length:])
-    paths_embedded_vector = paths_cropped.apply(one_hot_encode_into_vector, args=(encoding_dic, vector_length))
-    return paths_embedded_vector
+    return embedded_vector
 
 
 if __name__ == '__main__':
-    vector_length = 30
-    filename = f'added_embedded_vectors_{vector_length}'
+
     conversion_paths = pd.read_pickle('../../data/02_intermediate/cleaned.pkl')
-    conversion_paths['utm_medium_embedded'] = transform_utm_into_vector(
-        conversion_paths.utm_medium_list, one_hot_encode_into_flat_vector, ENCODING_DIC_UTM_MEDIUM, vector_length)
-    conversion_paths['utm_medium_embedded_2d'] = transform_utm_into_vector(
-        conversion_paths.utm_medium_list, one_hot_encode_into_two_dim_vector, ENCODING_DIC_UTM_MEDIUM, vector_length)
-    filepath = f'../../data/03_processed/{filename}.pkl'
+
+    conversion_paths['utm_source_embedded_1d'] = conversion_paths.apply(transform_into_vector, axis=1,
+                                                    args=('utm_source_list', ENCODING_DIC_UTM_SOURCE, False, False))
+    conversion_paths['utm_source_embedded_1d_with_time'] = conversion_paths.apply(transform_into_vector, axis=1,
+                                                    args=('utm_source_list', ENCODING_DIC_UTM_SOURCE, False, True))
+    conversion_paths['utm_source_embedded_2d'] = conversion_paths.apply(transform_into_vector, axis=1,
+                                                    args=('utm_source_list', ENCODING_DIC_UTM_SOURCE, True, False))
+    conversion_paths['utm_source_embedded_2d_with_time'] = conversion_paths.apply(transform_into_vector, axis=1,
+                                                    args=('utm_source_list', ENCODING_DIC_UTM_SOURCE, True, True))
+
+    conversion_paths['utm_medium_embedded_1d'] = conversion_paths.apply(transform_into_vector, axis=1,
+                                                    args=('utm_medium_list', ENCODING_DIC_UTM_MEDIUM, False, False))
+    conversion_paths['utm_medium_embedded_1d_with_time'] = conversion_paths.apply(transform_into_vector, axis=1,
+                                                    args=('utm_medium_list', ENCODING_DIC_UTM_MEDIUM, False, True))
+    conversion_paths['utm_medium_embedded_2d'] = conversion_paths.apply(transform_into_vector, axis=1,
+                                                    args=('utm_medium_list', ENCODING_DIC_UTM_MEDIUM, True, False))
+    conversion_paths['utm_medium_embedded_2d_with_time'] = conversion_paths.apply(transform_into_vector, axis=1,
+                                                    args=('utm_medium_list', ENCODING_DIC_UTM_MEDIUM, True, True))
+
+    filepath = f'../../data/03_processed/{FILENAME}.pkl'
     conversion_paths.to_pickle(filepath)
     print(f'Saved file to: {filepath}')
