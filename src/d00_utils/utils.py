dd57fef70ad93d1e1b2233581f3c1ff5476ab587
wiktor779 <wiktor.bieda@gmail.com>
2020-10-27 10:12:55 +0100

    added option to select the column from which we want to remove the value

--- src/d00_utils/utils.py
+++ src/d00_utils/utils.py
@@ -24,18 +24,18 @@ def split_data_into_train_and_test(df):
     return df_train, df_test
 
 
-def _remove_from_path(row, utm_to_remove, columns_to_clean):
-    indexes_to_be_removed = [i for i in range(len(row['utm_medium_list'])) if row['utm_medium_list'][i] == utm_to_remove]
+def _remove_from_path(row, utm_to_remove, columns_to_clean, utm_column_name):
+    indexes_to_be_removed = [i for i in range(len(row[utm_column_name])) if row[utm_column_name][i] == utm_to_remove]
     for column in columns_to_clean:
         for index in sorted(indexes_to_be_removed, reverse=True):
             del row[column][index]
 
 
-def remove_channel_from_path(df, channel_to_remove):
+def remove_channel_from_path(df, utm_to_remove, utm_column_name):
     df = df.copy()
     columns_to_clean = ['utm_medium_list', 'utm_source_list', 'days_till_conversions_list', 'event_dates_list',
                         'event_timestamps_list']
-    df.apply(_remove_from_path, axis=1, args=(channel_to_remove, columns_to_clean))
+    df.apply(_remove_from_path, axis=1, args=(utm_to_remove, columns_to_clean, utm_column_name))
     df = df[df.utm_medium_list.map(len) > 0]
     return df
 
