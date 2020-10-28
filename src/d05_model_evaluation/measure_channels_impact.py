2f1047c1174ed532f81226b3b0323375367c23cd
wiktor779 <wiktor.bieda@gmail.com>
2020-10-28 14:38:02 +0100

    solved problem with deep copy, with mutable values inside dataframe

--- src/d05_model_evaluation/measure_channels_impact.py
+++ src/d05_model_evaluation/measure_channels_impact.py
@@ -1,4 +1,5 @@
 from collections import OrderedDict
+import pickle
 from src.d00_utils.utils import remove_channel_from_path, get_unique_values
 
 
@@ -9,13 +10,20 @@ def _to_percentage(channel_impact_dict):
     return channel_impact_dict
 
 
-def measure_channels_impact(df, predict_revenue_func):
+def _deep_copy(df):
+    """ I made a mistake at beginning of the project by inserting mutable object into DataFrame which is anti-patterns.
+    Because i don't have time to correct it, this is workaround """
+    return pickle.loads(pickle.dumps(df))
+
+
+def measure_channels_impact(df, predict_revenue_func, utm_column_name):
     channels_revenue_dict = {}
     initial_revenue_sum = sum(df.revenue)
-    channels = get_unique_values(df['utm_medium_list'])
+    channels = get_unique_values(df[utm_column_name])
     for channel in channels:
-        conversion_paths_test_without_channel = remove_channel_from_path(df, channel)
-        revenue_predicted_without_channel = predict_revenue_func(conversion_paths_test_without_channel)
+        df_copy = _deep_copy(df)
+        conversion_paths_test_without_channel = remove_channel_from_path(df_copy, channel, utm_column_name)
+        revenue_predicted_without_channel = predict_revenue_func(conversion_paths_test_without_channel, utm_column_name)
         channels_revenue_dict[channel] = initial_revenue_sum - sum(revenue_predicted_without_channel)
     channels_revenue_dict = OrderedDict(sorted(channels_revenue_dict.items()))
     channels_revenue_dict = _to_percentage(channels_revenue_dict)
