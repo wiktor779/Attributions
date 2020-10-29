a76716367dc388fe6f99007ea35174347c4c2f99
wiktor779 <wiktor.bieda@gmail.com>
2020-10-29 10:55:13 +0100

    refactor plus added one hot encoding for utm source

--- src/d04_modelling/model_dense_predict.py
+++ src/d04_modelling/model_dense_predict.py
@@ -1,27 +1,30 @@
 import pandas as pd
+import re
 from keras.models import load_model
 from src.d00_utils.utils import split_data_into_train_and_test
 from src.d05_model_evaluation.measure_results import measure_results
 from src.d05_model_evaluation.measure_channels_impact import measure_channels_impact
 from src.d07_visualisation.visualise_channel_impact import visualize_channel_impact
 
-model_name = 'dense_model_small'
+vector_length = 5
+column_name = 'utm_source_embedded_1d_with_time'
+model_name = f'dense_{column_name}__vec_length_{vector_length}'
 
 
-def dense_predict(df):
-    x_test = df['utm_medium_embedded'].to_list()
+def dense_predict(df, utm_column_name):
+    x_test = df[column_name].to_list()
     model_dense = load_model(f'../../data/04_models/{model_name}.h5')
     return model_dense.predict(x_test).ravel().tolist()
 
 
 if __name__ == "__main__":
-    conversion_paths = pd.read_pickle('../../data/03_processed/added_embedded_vector.pkl')
+    conversion_paths = pd.read_pickle(f'../../data/03_processed/added_embedded_vectors_{vector_length}.pkl')
     conversion_paths_train, conversion_paths_test = split_data_into_train_and_test(conversion_paths)
 
-    revenue_predicted = dense_predict(conversion_paths_test)
+    revenue_predicted = dense_predict(conversion_paths_test, column_name)
     rmse = measure_results(conversion_paths_test.revenue, revenue_predicted)
     print(f'Dense\tRMSE: {rmse}')
 
-    channels_impact = measure_channels_impact(conversion_paths_test, dense_predict)
+    channels_impact = measure_channels_impact(conversion_paths_test, dense_predict, f'{column_name[:10]}_list')
     visualize_channel_impact(channels_impact, model_name)
     print(f'Channels impact in percentage {channels_impact}')
--- src/d04_modelling/model_dense_train.py
+++ src/d04_modelling/model_dense_train.py
@@ -4,12 +4,14 @@ from keras.models import Sequential
 from src.d00_utils.utils import split_data_into_train_and_test
 from src.d05_model_evaluation.measure_results import measure_results
 
-model_name = 'dense_model_small'
+vector_length = 40
+column_name = 'utm_source_embedded_1d_with_time'
+model_name = f'dense_{column_name}__vec_length_{vector_length}'
 
 
-def create_model_dense():
+def create_model_dense(input_length):
     model = Sequential()
-    model.add(Dense(60, activation='relu', input_shape=(60,)))
+    model.add(Dense(60, activation='relu', input_shape=(input_length,)))
     model.add(Dense(10, activation='relu'))
     model.add(Dense(1))
     model.compile(optimizer="rmsprop", loss="mse", metrics=['mse', 'mae'])
@@ -17,15 +19,15 @@ def create_model_dense():
 
 
 if __name__ == "__main__":
-    conversion_paths = pd.read_pickle('../../data/03_processed/added_embedded_vector.pkl')
+    conversion_paths = pd.read_pickle(f'../../data/03_processed/added_embedded_vectors_{vector_length}.pkl')
     conversion_paths_train, conversion_paths_test = split_data_into_train_and_test(conversion_paths)
 
-    x_train = conversion_paths_train['utm_medium_embedded'].to_list()
+    x_train = conversion_paths_train[column_name].to_list()
     y_train = conversion_paths_train.revenue.to_list()
-    x_test = conversion_paths_test['utm_medium_embedded'].to_list()
+    x_test = conversion_paths_test[column_name].to_list()
     y_test = conversion_paths_test.revenue.to_list()
 
-    model_dense = create_model_dense()
+    model_dense = create_model_dense(len(x_train[0]))
     model_dense.fit(x_train, y_train, batch_size=32, epochs=5, validation_data=(x_test, y_test))
     filepath = f'../../data/04_models/{model_name}.h5'
     model_dense.save(filepath)
